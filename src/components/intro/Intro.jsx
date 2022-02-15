775ebb95b89b4dbb48759eabf720a1f2cdebae77
mibrahim234 <mohamm1417@gmail.com>
2022-02-14 22:59:36 -0500

    profile pic, title

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,19 @@ import "./intro.scss"
 export default function intro() {
   return (
     <div className='intro' id="intro">
-      
+      <div className="left"></div>
+      <div className="imgContainer">
+        <img src="assets/man.png" alt="Ibrahim Cheffing It Up" />     </div>
+      <div className="right"> 
+      <div className="wrapper">
+        <h2>Hi There I'm </h2>
+        <h1>Mohammad Ibrahim</h1>
+        <h3>Full Stack Web Developer</h3>
+      </div>
+      {/* <a href="#portfolio">  
+        <img src="assets/avatar.jpg" alt="" />
+      </a> */}
+      </div>
     </div>
   )
 }
