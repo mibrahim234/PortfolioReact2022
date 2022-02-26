fbb274560fc8e920944f74d271b33950b5dbaf87
mibrahim234 <mohamm1417@gmail.com>
2022-02-26 00:58:52 -0500

    resume linked fixed

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -4,11 +4,12 @@ export default function Resume() {
   return (
     <div className='resume' id="resume">
       <div className="container">
-        <button className="btn"><h1>DOWNLOAD RESUME</h1></button>
+        <button className="btn">
+        <h1> <a href="https://docs.google.com/document/d/1l0bFaGLXDHbWvYSvLUIEP_aTt9NrrFo5/edit?usp=sharing&ouid=110479990162362107721&rtpof=true&sd=true
+ " target="_blank">DOWNLOAD RESUME</a></h1> </button>
         </div>
     </div>
   )
 }
-
 
 
