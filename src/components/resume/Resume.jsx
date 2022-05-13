f611717359d430321b104b319e14d0e1486b8904
mibrahim234 <mohamm1417@gmail.com>
2022-05-13 00:50:01 -0400

    resume part finished

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -2,13 +2,9 @@ import "./resume.scss"
 
 export default function Resume() {
   return (
-    <div className='resume' id="resume">
-      <div className="container">
-        <button className="btn">
-        <h1> <a href="https://docs.google.com/document/d/1l0bFaGLXDHbWvYSvLUIEP_aTt9NrrFo5/edit?usp=sharing&ouid=110479990162362107721&rtpof=true&sd=true
- " target="_blank">DOWNLOAD RESUME</a></h1> </button>
-        </div>
-    </div>
+    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh', backgroundColor: "white"}}>
+<h1> <a href="https://docs.google.com/document/d/1l0bFaGLXDHbWvYSvLUIEP_aTt9NrrFo5/edit?usp=sharing&ouid=110479990162362107721&rtpof=true&sd=true
+  " target="_blank">DOWNLOAD RESUME</a></h1> </div>
   )
 }
 
